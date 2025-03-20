3ef6b760393169d795a141b6b2bb013b24fe43da
danushka-weerasinghe <danushka@vega.lk>
2025-03-20 21:03:26 +0530

    all update from the diplay_old
    this one is the total merge from the ina229 and gpio expander also working with can setup all voltages using function calling.this code was tested and able to get reading from ina229 @PavanKVEGA @kavirunims use this one for the next function update

---
+++ Core/Inc/main_data.h
@@ -0,0 +1,19 @@
+/*
+ * main_data.h
+ *
+ *  Created on: Feb 28, 2025
+ *      Author: Asus
+ */
+
+#ifndef INC_MAIN_DATA_H_
+#define INC_MAIN_DATA_H_
+
+
+//to fix the fixed resistance(resistance that will be available at powerup fot all the cards)
+//#define start_Resistance_fix
+
+
+
+
+
+#endif /* INC_MAIN_DATA_H_ */
